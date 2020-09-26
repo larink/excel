d9e13a2d6f58180c381f9aca6e259c8b48aa35b4
larink <kirya.larin.1999@mail.ru>
2020-09-26 17:47:03 +0400

    Finish table resize

---
+++ src/components/table/table.template.js
@@ -0,0 +1,62 @@
+const CODES={
+  A: 65,
+  Z: 90
+}
+
+function toCell(_, col) {
+  return `
+        <div class="cell" contenteditable data-col="${col}"></div>
+    `
+}
+
+function toColumn(col, index) {
+  return `
+        <div class="column" data-type="resizable" data-col="${index}">
+            ${col}
+            <div class="col-resize" data-resize="col"></div>
+        </div>
+    `
+}
+
+function createRow(content, index) {
+  const resize= index ? '<div class="row-resize" data-resize="row"></div>' : ''
+  return `
+        <div class="row" data-type="resizable">
+            <div class="row-info">
+              ${index ? index : ''}
+              ${resize}
+            </div>
+            <div class="row-data">
+              ${content}
+            </div>
+        </div>
+    `
+}
+
+function toChar(el, index) {
+  return String.fromCharCode(CODES.A+index)
+}
+
+export function createTable(rowsCount=15) {
+  const colsCount=CODES.Z-CODES.A+1
+  const rows=[]
+
+  const cols=new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('')
+
+  rows.push(createRow(cols, null))
+
+
+  for (let i=0; i<rowsCount; i++) {
+    const cells=new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+
+    rows.push(createRow(cells, i+1))
+  }
+  return rows.join('')
+}
